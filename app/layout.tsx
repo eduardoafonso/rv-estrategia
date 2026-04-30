bb3a4343ef051145226f0b8484a9720dc41b8121
Eduardo Afonso <contato@eduardoafonso.com>
2026-04-30 20:50:16 -0300

    Google Analitcs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import Script from "next/script";
+
 export default function RootLayout({
     children,
 }: {
@@ -5,6 +7,20 @@ export default function RootLayout({
 }) {
     return (
         <html lang="pt-BR">
+            <head>
+                <Script
+                    src={`https://www.googletagmanager.com/gtag/js?id=G-H1QVFKNHJQ`}
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-H1QVFKNHJQ');
+          `}
+                </Script>
+            </head>
             <body>{children}</body>
         </html>
     );
