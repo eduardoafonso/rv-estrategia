d15e1c22d8ee9bf3ac05697e20ca559b72434617
Eduardo Afonso <contato@eduardoafonso.com>
2026-04-30 22:38:47 -0300

    Correção dupla req GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,25 +69,30 @@ export default function RootLayout({
 }) {
     return (
         <html lang="pt-BR">
-            <head>
+            <body>
+                {children}
+
+                {/* GA SEM HEAD MANUAL */}
                 {GA_ID && (
                     <>
                         <Script
                             src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
                             strategy="afterInteractive"
                         />
+
                         <Script id="ga" strategy="afterInteractive">
                             {`
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', '${GA_ID}');
+                gtag('config', '${GA_ID}', {
+                  page_path: window.location.pathname,
+                });
               `}
                         </Script>
                     </>
                 )}
-            </head>
-            <body>{children}</body>
+            </body>
         </html>
     );
 }
